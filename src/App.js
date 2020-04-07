4ba11e0de92f49cd871598967b7be207c5f38661
koustub manchiraju <koustub.247@gmail.com>
2020-04-07 13:51:59 -0400

    changes to routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div class="container">
-      <Route exact path={["/","/Home"]} component={HomePage} />
+      <Route exact path={["/","/Home","/reactportfolio"]} component={HomePage} />
       <Route path="/Projects" component={Projects} />
       </div>
       <Footer />
