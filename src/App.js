8ade2fedaffb2679c6496084a65f2db2fb20873e
koustub manchiraju <koustub.247@gmail.com>
2020-04-07 20:15:07 -0400

    adding home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter as Router,Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import HomePage from './Components/Home';
 import Projects from './Components/Project';
 import Footer from './Components/footer';
 function App() {
   return (
-    <Router >
+    <Router>
     <div className="App">
       <Navbar />
       <div class="container">
-      <Route exact path={["/","/Home"]} component={HomePage} />
+      <Route exact path={["/","/Home","/reactportfolio"]} component={HomePage} />
       <Route path="/Projects" component={Projects} />
       </div>
       <Footer />
