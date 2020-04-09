307d1915b21871aac5870a5449d38db2f56378e7
koustub manchiraju <koustub.247@gmail.com>
2020-04-08 22:53:34 -0400

    Updates

---
+++ precache-manifest.54898bf0c1b8dc8fb8dad5b0a8c5ecdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0e005cd21c773dd6a12fc852f9e562",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "f18052d115a83728bbab",
+    "url": "/reactportfolio/static/js/2.216c1ac8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.216c1ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb173eb500008335208f",
+    "url": "/reactportfolio/static/js/main.d6ae60ea.chunk.js"
+  },
+  {
+    "revision": "f8ca34d2a84711bf872c",
+    "url": "/reactportfolio/static/js/runtime-main.0c1598a4.js"
+  }
+]);
